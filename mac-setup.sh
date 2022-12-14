b2306261d24df0a3c0c2b23e532de3e3591b1df2
Kevin Centeno <kevcenteno@users.noreply.github.com>
2022-12-14 15:47:18 -0500

    Update mac setup

--- mac-setup.sh
+++ mac-setup.sh
@@ -5,26 +5,11 @@ sh ./symlink.sh
 
 # Common Formulae
 echo "Installing common formulae"
-brew install chruby ruby-install zsh node vim tmux wget the_silver_searcher
-sudo mv /usr/bin/vim /usr/bin/vim72
-
-# change default shell to zsh
-echo "Change default shell to zsh"
-chsh -s /usr/local/bin/zsh $USER
-
-# ruby versions
-echo "Ruby install 2.1.2"
-ruby-install ruby 2.1.2
-
-# node packages
-echo "Installing yeoman and angular generator"
-npm install -g yo
-npm install -g generator-angular
-npm install -g trash
+brew install tmux wget
 
 # oh my zsh
 echo "Installing oh my zsh"
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 echo "#######"
 echo "#######"
