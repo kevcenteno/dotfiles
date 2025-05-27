860cee1843d84b983d897da2b4800c46aed4ca91
kevcenteno <kevin@oldeyorke.ca>
2025-05-27 09:53:47 -0400

    Add wezterm config

---
+++ prefs/wezterm.lua
@@ -0,0 +1,85 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Argonaut (Gogh)'
+
+config.audible_bell = 'Disabled'
+
+config.use_fancy_tab_bar = false
+config.tab_max_width = 100
+
+config.font = wezterm.font("Hack", {weight="Regular", stretch="Normal", style="Normal"}) -- /usr/share/fonts/truetype/hack/Hack-Regular.ttf, FontConfig
+config.font_size = 10
+
+config.default_prog = { 'zsh', '-l' }
+
+local act = wezterm.action
+config.keys = {
+  {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, _, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+  { key = "Insert", mods = "SHIFT", action = act.PasteFrom("Clipboard") },
+}
+
+config.mouse_bindings = {
+  -- Change the default click behavior so that it only selects
+  -- text and doesn't open hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'NONE',
+    action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+  },
+
+  -- and make CTRL-Click open hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = act.OpenLinkAtMouseCursor,
+  },
+  -- NOTE that binding only the 'Up' event can give unexpected behaviors.
+  -- Read more below on the gotcha of binding an 'Up' event only.
+}
+
+wezterm.on('update-status', function(window)
+  -- Grab the utf8 character for the "powerline" left facing
+  -- solid arrow.
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+  -- Grab the current window's configuration, and from it the
+  -- palette (this is the combination of your chosen colour scheme
+  -- including any overrides).
+  local color_scheme = window:effective_config().resolved_palette
+  local bg = color_scheme.background
+  local fg = color_scheme.foreground
+
+  window:set_right_status(wezterm.format({
+    -- First, we draw the arrow...
+    { Background = { Color = 'none' } },
+    { Foreground = { Color = bg } },
+    { Text = SOLID_LEFT_ARROW },
+    -- Then we draw our text
+    { Background = { Color = bg } },
+    { Foreground = { Color = fg } },
+    { Text = ' ' .. wezterm.hostname() .. ' ' },
+  }))
+end)
+
+-- and finally, return the configuration to wezterm
+return config
