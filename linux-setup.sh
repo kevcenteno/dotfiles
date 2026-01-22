8cdda7f3fc7ce50c09076facd048c947e2ba168c
kevcenteno <kevin@oldeyorke.ca>
2026-01-22 09:42:24 -0500

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,70 +1,47 @@
 #!/bin/bash
 
-sh ./symlink.sh
-
 # Install the things
-sudo apt-get update
-sudo apt-get -y dist-upgrade
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make libssl-dev pkg-config build-essential curl gcc vim xsel tmux zsh ncurses-term nodejs silversearcher-ag
-
-# Colors
-echo "export TERM=xterm-256color" >> $HOME/.bashrc
-source $HOME/.bashrc
-echo "source $HOME/.bashrc" >> $HOME/.bash_profile
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update && sudo apt-get install -y \
+  build-essential \
+  cmake \
+  curl \
+  fzf \
+  g++ \
+  gcc \
+  libssl-dev \
+  jq \
+  make \
+  ncurses-term \
+  neovim \
+  pkg-config \
+  python3 \
+  python3-pip \
+  python3-venv \
+  ripgrep \
+  software-properties-common \
+  tmux \
+  vim \
+  zsh
 
 # oh my zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-# Grunt
-sudo npm install -g grunt-cli
+# Create the custom themes directory if it doesn't exist
+mkdir -p ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes
 
-# chruby
-wget -O chruby-0.3.8.tar.gz https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz
-tar -xzvf chruby-0.3.8.tar.gz
-cd chruby-0.3.8/
-sudo make install
-cd ../ && rm -rf chruby-0.3.8 chruby-0.3.8.tar.gz
+# Download the bullet-train theme
+curl -L http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -o ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/bullet-train.zsh-theme
 
-# Ruby Build
-wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz
-tar -xzvf ruby-install-0.4.3.tar.gz
-cd ruby-install-0.4.3/
-sudo make install
-cd ../ && rm -rf ruby-install-0.4.3 ruby-install-0.4.3.tar.gz
+# Symlink config files
+sh ./symlink.sh
 
-# Trash
-sudo npm install -g trash
-
-# Prompt to install Ruby
-function install_ruby {
-  ruby-install ruby $1;
-  echo "chruby ruby-$1" >> $HOME/.zshrc;
-}
-
-function what_version {
-  while true; do
-    read -p "Which version?  Please enter a specific version number.  Type 'q' to exit: " version
-    case $version in
-      [q]* ) exit;;
-      * ) install_ruby $version; break;;
-    esac
-  done
-}
-
-while true; do
-  read -p "Do you want to install Ruby? [y/n]: " yn
-  case $yn in
-    [Yy]* ) what_version; break;;
-    [Nn]* ) exit;;
-    * ) echo "Please answer y or n.";;
-  esac
-done
+sudo chsh -s $(which zsh) $(whoami)
 
 echo ">>>"
 echo ">>> Installed the things"
 echo ">>>"
 
-
+exec zsh
